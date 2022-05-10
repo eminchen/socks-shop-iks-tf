07dc3afa5a659ad6087c065d4fc8966fc033117d
eminchen <eminchen@cisco.com>
2022-05-10 13:08:52 -0400

    Update variables.tf

--- iks-sockshop/variables.tf
+++ iks-sockshop/variables.tf
@@ -1,16 +1,19 @@
 variable "intersight_key" {
   type =  string
- 
-}  
+}
+
 variable "intersight_secret" {
   type  = string
 }
+
 variable "endpoint" {
   default = "https://www.intersight.com"
 }
+
 variable "IKSCluster" {
   type = string
 }
+
 variable "namespace" {
   type = string
   description = "Namespace to install application"
