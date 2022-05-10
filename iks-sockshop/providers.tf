3663f48d3337542a9446f1e69519ca8cc00585ad
eminchen <eminchen@cisco.com>
2022-05-10 13:09:34 -0400

    Update providers.tf

--- iks-sockshop/providers.tf
+++ iks-sockshop/providers.tf
@@ -9,7 +9,6 @@ data "intersight_kubernetes_cluster" "IKSCluster" {
 }
 
 provider "kubernetes" {
-
   host                   = yamldecode(base64decode(data.intersight_kubernetes_cluster.IKSCluster.results.0.kube_config)).clusters.0.cluster.server
   cluster_ca_certificate = base64decode(yamldecode(base64decode(data.intersight_kubernetes_cluster.IKSCluster.results.0.kube_config)).clusters.0.cluster.certificate-authority-data)
   client_certificate     = base64decode(yamldecode(base64decode(data.intersight_kubernetes_cluster.IKSCluster.results.0.kube_config)).users[0].user.client-certificate-data)
@@ -27,6 +26,7 @@ provider "helm" {
     client_key             = base64decode(yamldecode(base64decode(data.intersight_kubernetes_cluster.IKSCluster.results.0.kube_config)).users[0].user.client-key-data)
   }
 }
+
 data "kubernetes_service" "socks-lb" {
   metadata {
     namespace = var.namespace
