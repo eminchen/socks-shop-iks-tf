0f7da990d4a848169669ef1d200ebb2e333b8f27
eminchen <eminchen@cisco.com>
2022-05-11 06:49:52 -0400

    Update helm_release_socks.tf

--- iks-sockshop/helm_release_socks.tf
+++ iks-sockshop/helm_release_socks.tf
@@ -2,7 +2,7 @@ resource "helm_release" "socks-demo" {
   name = "socks-demo"
   chart = "./helm-chart"
   namespace = var.namespace
-  create_namespace = true
+  create_namespace = false
   timeout = 600
 
 //set {
@@ -22,6 +22,6 @@ resource "helm_release" "socks-demo" {
     value = "secretpassword"
   }
 */
-#depends_on = [kubernetes_namespace.sock_ns]
+depends_on = [kubernetes_namespace.sock_ns]
 
 }
